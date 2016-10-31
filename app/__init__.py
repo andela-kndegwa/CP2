a05232ac4ee523c8fb3c109fe4b962d401020b07
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-11-01 02:11:28 +0300

    (Chore) Reorganises file structure to suit project

---
+++ app/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask, Blueprint
+from flask_restplus import Api
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+db = SQLAlchemy()  # Initialise the db
+api_bp = Blueprint('api', __name__, url_prefix='/v1.0')
+
+# Defining flask_restplus parameters
+version = '1.0'
+title = 'BLister'
+description = 'BLister is a bucketlist application API'\
+    ' built with Flask and Swagger UI '
+
+# Initialise the Api class
+api = Api(version=version, title=title,
+          description=description)
+
+
+def create_app(config_name):
+    '''
+    The creat_app function functions as the application
+    factory. This means all the initialization is effected
+    here.
+
+    Arguments :
+    It takes the config dictionary from the config.py
+    and accesses the required class and its properties as
+    requried.
+
+    Return value:
+    It finally returns the app fully created
+
+    '''
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    db.init_app(app)
+    app.register_blueprint(api_bp)
+    return app
---
+++ app/config.py
@@ -0,0 +1,47 @@
+import os
+import random
+import string
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    '''
+    This is the main configuration class that allows
+    the production, development and testing databases to
+    be created and configured from scratch with appropriate settings.
+    '''
+    SECRET_KEY = ''.join(random.choice(
+        string.ascii_uppercase + string.digits)for x in xrange(32))
+    # The above generates a random and hard to guess string
+    # and stores it in the SECRET_KEY variable.
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'blister-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'blister-test.sqlite')
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'blister.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
+# The above allows us to import all the above configuration in one
+# dictionary.
